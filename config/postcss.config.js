685af904f605655b6c240aa438fdba2b11c62949
Henk Verlinde <henk@henkverlinde.com>
2026-02-23 18:10:11 +0100

    Drop whitelister

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,8 +1,8 @@
-const autoprefixer = require('autoprefixer');
-const { purgeCSSPlugin } = require('@fullhuman/postcss-purgecss');
-const whitelister = require('purgecss-whitelister');
+import autoprefixer from 'autoprefixer';
+import purgeCSSPlugin from '@fullhuman/postcss-purgecss';
+// import whitelister from 'purgecss-whitelister';
 
-module.exports = {
+export default {
     plugins: [
         autoprefixer(),
         purgeCSSPlugin({
@@ -57,7 +57,7 @@ module.exports = {
                 'page-item',
                 'page-link',
                 'not-content',
-                ...whitelister(['./assets/scss/**/*.scss', './node_modules/@thulite/doks-core/assets/scss/components/_code.scss', './node_modules/@thulite/doks-core/assets/scss/components/_expressive-code.scss', './node_modules/@thulite/doks-core/assets/scss/common/_syntax.scss'])
+                // ...whitelister(['./assets/scss/**/*.scss', './node_modules/@thulite/doks-core/assets/scss/components/_code.scss', './node_modules/@thulite/doks-core/assets/scss/components/_expressive-code.scss', './node_modules/@thulite/doks-core/assets/scss/common/_syntax.scss'])
             ]
         })
     ]
